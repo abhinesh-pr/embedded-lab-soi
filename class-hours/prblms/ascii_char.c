1557d963f73bc4b4fa9df002309fc82c4e7d1d37
Abhinesh P R <139995888+abhinesh-pr@users.noreply.github.com>
2025-02-05 16:18:27 +0530

    Delete class-hours/prblms directory

--- class-hours/prblms/ascii_char.c
+++
@@ -1,6 +0,0 @@
-// 4.Print ASCII of character
-#include <stdio.h>
-int main(){
-    char ch='A';
-    printf("The ASCII of %c is %d",ch,ch);
-}
--- class-hours/prblms/your_name.c
+++
@@ -1,7 +0,0 @@
-//1. C program to print your name
-#include <stdio.h>
-
-int main(){
-    printf("Your name is Abhinesh");
-    return 0;
-}
